4ce05923d5a22c9ca205a08f8a405d7fcbec104c
Yashvalsange <yashvalsange004@gmail.com>
2026-07-22 15:31:24 +0530

    Add program for float and integer conversion

---
+++ MITWPU/sem-3/OOP/prac-3.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1,num2;
+    float n2,num1;
+    cout<<"enter a float number=";
+    cin>>num1;
+      cout<<"enter a integer number=";
+    cin>>num2;
+     n1=int(num1);
+     n2=float(num2);
+      cout<<"conversion of float to integer is="<<n1<<endl;
+       cout<<"conversion of integer to float is="<<n1;
+       return 0;
+}
